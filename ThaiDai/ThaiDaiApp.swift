b888add3c4ec6f25d2d754146a2476e4eb2690ef
DevboiDesigns <visrule@protonmail.com>
2023-08-11 15:37:02 +0700

    Initial Commit

---
+++ ThaiDai/ThaiDaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThaiDaiApp.swift
+//  ThaiDai
+//
+//  Created by Christopher Hicks on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ThaiDaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
