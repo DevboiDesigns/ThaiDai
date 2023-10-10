19c581ceec7a8bd412877a6cc1aeb64c71eecaae
DevboiDesigns <visrule@protonmail.com>
2023-10-11 00:34:12 +0700

    cleanup

--- ThaiDai/Network/TDError.swift
+++ ThaiDai/Network/TDError.swift
@@ -14,11 +14,8 @@ enum TDError: String, Error {
     case invalidResponse = "Well thats weird...invalid response from server, please try again."
     case invalidData = "The data received from the server was invalid .... were working on it."
     case invalidURL = "Sorry the request had an issue getting data back, try again shortly."
-    case resetLinkedAccount = "Your credentials have changed with your account, we need you to reconnect your Institution."
     case searchQueryIssue = "Oops... maybe try a different spelling"
     case invalidCredentials = "Sorry your credentials are invalid. Please email reactivate@stocklift.co for further help."
-    case incorrectPurchaseAmount = "This purchase amount exceeds your cash availability. Please select a new amount."
-    case contestOver = "Check back shortly, you can begin adding stocks for next week’s round after 5:15pm EST."
     case duplicateAccount = "Oops it looks like you have already linked one of these accounts, try again with a new account."
 
 }
